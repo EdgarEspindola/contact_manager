ea7ae9eb9b0e060bf746613eb6bdaea35e057758
Edgar <edgareduardoespindola@gmail.com>
2018-12-07 20:14:45 -0600

    Created some data for groups and contacts table usign database seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181208012337) do
+ActiveRecord::Schema.define(version: 20181208013031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20181208012337) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "name", limit: 100
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "contacts", "groups"
